35efab9c07e7fe1bf65560f461819c84c2d7aeb9
Daniel Acevedo <69832074+dacevedo11@users.noreply.github.com>
2024-04-04 20:53:49 -0700

    finished code
    still needs reflection

--- experiment1/js/project.js
+++ experiment1/js/project.js
@@ -1,34 +1,52 @@
-// project.js - purpose and description here
-// Author: Your Name
-// Date:
-
-// NOTE: This is how we might start a basic JavaaScript OOP project
-
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-
-// define a class
-class MyProjectClass {
-  // constructor function
-  constructor(param1, param2) {
-    // set properties using 'this' keyword
-    this.property1 = param1;
-    this.property2 = param2;
-  }
-  
-  // define a method
-  myMethod() {
-    // code to run when method is called
-  }
-}
+// Experiment 1: Getting Started
+// Author: Daniel Acevedo
+// Date: 4.4.2024
 
 function main() {
-  // create an instance of the class
-  let myInstance = new MyProjectClass("value1", "value2");
 
-  // call a method on the instance
-  myInstance.myMethod();
+  const fillers = {
+    hairstyle: ["Bald", "Crew cut", "Side part", "Receding hairline", "Dr. Phil", "Flat top"],
+    top: ["Hawaiian shirt", "Striped polo shirt", "Tye Dye shirt", "American flag tshirt", "Metallica shirt", "Work jacket", "White tshirt"],
+    bottom: ["Cargo shorts", "Jean shorts", "Gray sweatpants", "Blue jeans", "Khakis", "Basketball shorts", "Cargo pants"],
+    accessory: ["Fanny pack", "Baseball cap", "Bucket hat", "Oakley sunglasses", "Suspenders", "Apron"],
+    footwear: ["White New Balances", "Merrells", "Nike Air Monarchs", "Crocs", "Socks & Sandals", "Boots"],
+  };
+
+  const template = ` <br>
+  Hairstyle: $hairstyle <br> <br>
+  Top: $top <br> <br>
+  Bottom: $bottom <br> <br>
+  Accessory: $accessory <br> <br>
+  Footwear: $footwear`;
+
+
+  // STUDENTS: You don't need to edit code below this line.
+
+  const slotPattern = /\$(\w+)/;
+
+  function replacer(match, name) {
+    let options = fillers[name];
+    if (options) {
+      return options[Math.floor(Math.random() * options.length)];
+    } else {
+      return `<UNKNOWN:${name}>`;
+    }
+  }
+
+  function generate() {
+    let story = template;
+    while (story.match(slotPattern)) {
+      story = story.replace(slotPattern, replacer);
+    }
+
+    /* global box */
+    $("#box").html(story);
+  }
+
+  /* global clicker */
+  $("#clicker").click(generate);
+
+  generate();
 }
 
-// let's get this party started - uncomment me
-//main();
+main();
